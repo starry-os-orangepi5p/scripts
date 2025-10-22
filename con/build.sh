a4f75b36590e5a9ace01c53732d24972fd3a7f30
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-22 20:46:56 +0800

    Update starry tool

--- con/build.sh
+++ con/build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-ssh ${STARRY_SSH_HOST} -t "bash ${STARRY_ROOT}/scripts/dev/build.sh"
+ssh ${STARRY_SSH_HOST} \
+    -t "STARRY_ROOT=${STARRY_ROOT} \
+        STARRY_TOOL=${STARRY_TOOL} \
+        bash ${STARRY_ROOT}/scripts/dev/build.sh"
 scp ${STARRY_SSH_HOST}:${STARRY_ROOT}/starry-mix/starry-mix_aarch64-opi5p.uimg \
     ${STARRY_TFTP_PATH}/Uimage
--- dev/build.sh
+++ dev/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cd $STARRY_ROOT/starry-mix
+export PATH=${STARRY_TOOL}/x86_64-linux-musl-cross/bin:$PATH
+export PATH=${STARRY_TOOL}/aarch64-linux-musl-cross/bin:$PATH
+export PATH=${STARRY_TOOL}/riscv64-linux-musl-cross/bin:$PATH
+export PATH=${STARRY_TOOL}/loongarch64-linux-musl-cross/bin:$PATH
+
+cd ${STARRY_ROOT}/starry-mix
 make ARCH=aarch64 LOG=debug MODE=debug BACKTRACE=y opi5p
