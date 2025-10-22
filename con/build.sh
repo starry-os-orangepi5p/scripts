72379efecd5a3b145d7c4b81f51805900f2c0150
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-22 21:02:02 +0800

    Fix build.sh

--- con/build.sh
+++ con/build.sh
@@ -5,6 +5,7 @@ set -e
 ssh ${STARRY_SSH_HOST} \
     -t "STARRY_ROOT=${STARRY_ROOT} \
         STARRY_TOOL=${STARRY_TOOL} \
+        STARRY_BUILD=${STARRY_BUILD} \
         bash ${STARRY_ROOT}/scripts/dev/build.sh"
 # scp ${STARRY_SSH_HOST}:${STARRY_ROOT}/starry-mix/starry-mix_aarch64-opi5p.uimg \
 #     ${STARRY_TFTP_PATH}/Uimage
--- dev/build.sh
+++ dev/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export PATH=${STARRY_TOOL}/x86_64-linux-musl-cross/bin:$PATH
 export PATH=${STARRY_TOOL}/aarch64-linux-musl-cross/bin:$PATH
 export PATH=${STARRY_TOOL}/riscv64-linux-musl-cross/bin:$PATH
@@ -8,6 +10,7 @@ export PATH=${STARRY_TOOL}/loongarch64-linux-musl-cross/bin:$PATH
 cd ${STARRY_ROOT}/starry-mix
 make ARCH=aarch64 LOG=debug MODE=debug BACKTRACE=y opi5p
 cp ${STARRY_ROOT}/starry-mix/starry-mix_aarch64-opi5p.uimg ${STARRY_BUILD}
+cd ${STARRY_BUILD}
 git add ${STARRY_BUILD}/starry-mix_aarch64-opi5p.uimg
 git commit -m "Build starry-mix_aarch64-opi5p.uimg"
 git push
