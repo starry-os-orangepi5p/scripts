95c5a9b142ca46e3c6da638da0353b5ce70dc59d
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-24 17:30:09 +0800

    update build script

--- dev/build.sh
+++ dev/build.sh
@@ -7,10 +7,17 @@ export PATH=${STARRY_TOOL}/aarch64-linux-musl-cross/bin:$PATH
 export PATH=${STARRY_TOOL}/riscv64-linux-musl-cross/bin:$PATH
 export PATH=${STARRY_TOOL}/loongarch64-linux-musl-cross/bin:$PATH
 
+LOG=${LOG:-debug}
+MODE=${MODE:-debug}
+BACKTRACE=${BACKTRACE:-y}
+
 cd ${STARRY_ROOT}/starry-mix
-make ARCH=aarch64 LOG=debug MODE=debug BACKTRACE=y opi5p
-cp ${STARRY_ROOT}/starry-mix/starry-mix_aarch64-opi5p.uimg ${STARRY_BUILD}
-cd ${STARRY_BUILD}
-git add ${STARRY_BUILD}/starry-mix_aarch64-opi5p.uimg
-git commit -m "Build starry-mix_aarch64-opi5p.uimg"
-git push
+make ARCH=aarch64 LOG=${LOG} MODE=${MODE} BACKTRACE=${BACKTRACE} opi5p
+
+if [ "$1" = "push" ]; then
+    cp ${STARRY_ROOT}/starry-mix/starry-mix_aarch64-opi5p.uimg ${STARRY_BUILD}
+    cd ${STARRY_BUILD}
+    git add ${STARRY_BUILD}/starry-mix_aarch64-opi5p.uimg
+    git commit -m "Build starry-mix_aarch64-opi5p.uimg"
+    git push
+fi
