50d21fc9d572d3a57183c72f9978e1cf06d05297
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-24 17:40:58 +0800

    update build script on con

--- con/build.sh
+++ con/build.sh
@@ -2,11 +2,23 @@
 
 set -e
 
+LOG=${LOG:-debug}
+MODE=${MODE:-debug}
+BACKTRACE=${BACKTRACE:-y}
+
+PUSH_ARG=""
+if [ "$1" = "push" ]; then
+    PUSH_ARG="push"
+fi
+
 ssh ${STARRY_SSH_HOST} \
    -t "STARRY_ROOT=${STARRY_ROOT} \
        STARRY_TOOL=${STARRY_TOOL} \
        STARRY_BUILD=${STARRY_BUILD} \
-       bash ${STARRY_ROOT}/scripts/dev/build.sh"
+       LOG=${LOG} \
+       MODE=${MODE} \
+       BACKTRACE=${BACKTRACE} \
+       bash ${STARRY_ROOT}/scripts/dev/build.sh ${PUSH_ARG}"
 
 # use git repo instead of scp
 # scp ${STARRY_SSH_HOST}:${STARRY_ROOT}/starry-mix/starry-mix_aarch64-opi5p.uimg \
