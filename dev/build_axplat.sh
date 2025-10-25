c9d62289e77076d44ec4fcfce3d2f2195c3a3f5c
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-25 15:00:16 +0800

    fix

--- dev/build_axplat.sh
+++ dev/build_axplat.sh
@@ -12,13 +12,14 @@ MODE=${MODE:-release}
 APP=${APP:-rstiny}
 
 app_target=${APP}.bin
+opi5p_root=${STARRY_ROOT}/starry-mix/module-local/axplat-opi5p
 
-cd ${STARRY_ROOT}/starry-mix/module-local/axplat-opi5p
+cd ${opi5p_root}
 make APP=${APP} LOG=${LOG} MODE=${MODE} build
 make APP=${APP} uimage
 
 if [ "$1" = "push" ]; then
-    cp ${STARRY_ROOT}/axplat-opi5p/target/aarch64-unknown-none-softfloat/${MODE}/${app_target} ${STARRY_BUILD}
+    cp ${opi5p_root}/target/aarch64-unknown-none-softfloat/${MODE}/${app_target} ${STARRY_BUILD}
     cd ${STARRY_BUILD}
     git add ${STARRY_BUILD}/${app_target}
     git commit -m "Build ${app_target} for axplat-opi5p"
