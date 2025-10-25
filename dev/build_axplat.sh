9e767148f2520175b7fe45f647d09a7b1c4b6de0
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-25 14:51:31 +0800

    update uimage support

--- dev/build_axplat.sh
+++ dev/build_axplat.sh
@@ -11,13 +11,16 @@ LOG=${LOG:-info}
 MODE=${MODE:-release}
 APP=${APP:-rstiny}
 
+app_target=${APP}.uimg
+
 cd ${STARRY_ROOT}/axplat-opi5p
 make APP=${APP} LOG=${LOG} MODE=${MODE} build
+make APP=${APP} uimage
 
 if [ "$1" = "push" ]; then
-    cp ${STARRY_ROOT}/axplat-opi5p/target/aarch64-unknown-none-softfloat/${MODE}/${APP}.bin ${STARRY_BUILD}
+    cp ${STARRY_ROOT}/axplat-opi5p/target/aarch64-unknown-none-softfloat/${MODE}/${app_target} ${STARRY_BUILD}
     cd ${STARRY_BUILD}
-    git add ${STARRY_BUILD}/${APP}.bin
-    git commit -m "Build ${APP}.bin for axplat-opi5p"
+    git add ${STARRY_BUILD}/${app_target}
+    git commit -m "Build ${app_target} for axplat-opi5p"
     git push
 fi
