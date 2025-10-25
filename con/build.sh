f5672d18571bcaac592397862f41c23455d9510c
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-25 16:32:07 +0800

    feat: add log captrue

--- con/build.sh
+++ con/build.sh
@@ -25,3 +25,14 @@ ssh ${STARRY_SSH_HOST} \
     # ${STARRY_TFTP_PATH}/Uimage
 cd ${STARRY_CON_BUILD} && git pull
 cp ${STARRY_CON_BUILD}/starry-mix_aarch64-opi5p.uimg ${STARRY_TFTP_PATH}/Uimage
+
+# archive old log
+mkdir -p ${STARRY_LOG}/archive
+mv ${STARRY_LOG}/last_console*.log ${STARRY_LOG}/archive
+
+log_file=${STARRY_CON_LOG}/last_console$(date +%Y%m%d%H%M%S).log
+touch ${log_file}
+minicom -C ${log_file}
+git add ${log_file}
+git commit -m "Update logs"
+git push
